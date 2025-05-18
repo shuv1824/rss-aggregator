aa07b4a74163b5551bf8cde36becce9f7bac94c5
Shah Nawaz <shuvo1824@hotmail.com>
2025-05-18 23:35:33 +0600

    chore: create feed follow

--- handler_feed_follow.go
+++ handler_feed_follow.go
@@ -10,10 +10,9 @@ import (
 	"github.com/shuv1824/rss-aggregator/internal/database"
 )
 
-func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
 	type parameters struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
+		FeedID uuid.UUID `json:"feed_id"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -25,23 +24,22 @@ func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+	feedFollow, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
-		Name:      params.Name,
-		Url:       params.URL,
 		UserID:    user.ID,
+		FeedID:    params.FeedID,
 	})
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("couldn't create feed: %v", err))
+		respondWithError(w, 400, fmt.Sprintf("couldn't create feed follow: %v", err))
 		return
 	}
 
-	respondWithJSON(w, 201, databaseFeedToFeed(feed))
+	respondWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
 }
 
-func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Request) {
 	feeds, err := apiCfg.DB.GetAllFeeds(r.Context())
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("couldn't get feeds: %v", err))
